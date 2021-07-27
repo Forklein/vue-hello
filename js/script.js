0c9583240fce2682f74a055d6db3d073d900c3f0
Forklein <giuseppepisani2@gmail.com>
2021-07-27 14:25:11 +0200

    Set methods with @click for alert and input with v-model

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 Vue.config.devtools = true;
 console.log('Vue Activated', Vue);
+
+const root = new Vue({
+    el: '#container',
+    data: {
+        welcome: 'Hello Vue!',
+        image: 'https://www.netfarm.it/web/image/4973/vuejs.png',
+        message: {
+            name: '',
+            surname: '',
+            email: '',
+        },
+        colortext: 'green',
+    },
+    methods: {
+        bye() {
+            alert('Hello Vue!');
+        }
+    }
+});
